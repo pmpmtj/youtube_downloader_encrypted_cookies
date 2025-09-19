2e12dec0044c8dd50a956ea6e9012a8f0db6748c
pmpmt <pmpmtj@hotmail.com>
2025-09-19 11:03:00 +0100

    add config with download .mp3 option -> boolean

---
+++ core/shared_utils/app_config.py
@@ -0,0 +1,18 @@
+"""
+Application configuration module.
+
+"""
+
+import logging
+
+
+# Initialize logger for this module
+logger = logging.getLogger("app_config")
+
+# Application configuration for Youtube downloader
+APP_CONFIG = {
+    "audio": {
+        "save_to_mp3": "False"
+    }
+}
+
